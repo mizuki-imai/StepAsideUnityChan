21e84b1bff5795d715124108060db4ce929b7a5b
mizuki-imai <mi-pikachu.5658@docomo.ne.jp>
2019-06-26 20:56:51 +0900

    StepAsideUnityChan
    C#スクリプトのDestroyは、インターネットで調べて出てきたものですが、こちらはアタッチしていません。
    しかし勉強のため残したままにはしてあります。よろしくおねがいします。

---
+++ Assets/DeleteController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteController : MonoBehaviour {
+	//Unityちゃんのオブジェクト
+	private GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+		//Unityちゃんのオブジェクトを取得
+		this.unitychan = GameObject.Find ("unitychan");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Unityちゃんよりz座標が小さいとき
+		if (unitychan.transform.position.z > this.transform.position.z){
+			//アイテムを削除する
+			Destroy(this.gameObject);
+	}
+}
+}
---
+++ Assets/Destroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	//オブジェクトが画面外に出た時(ここでこまってます。)
+	void OnBecameInvisible(){
+		Destroy (this.gameObject);
+	}
+
+
+}
